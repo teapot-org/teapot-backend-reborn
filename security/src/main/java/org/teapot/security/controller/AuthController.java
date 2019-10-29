3d82aaaac046472114c2b5e2d56f0f6c53d3c2af
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-10-29 17:39:37 +0300

    refactored auth controller responses

--- security/src/main/java/org/teapot/security/controller/AuthController.java
+++ security/src/main/java/org/teapot/security/controller/AuthController.java
@@ -4,30 +4,24 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.*;
 import org.teapot.security.exception.BadRequestException;
 import org.teapot.security.model.AuthProvider;
 import org.teapot.security.model.User;
 import org.teapot.security.payload.AuthResponse;
 import org.teapot.security.payload.LoginRequest;
-import org.teapot.security.payload.Response;
 import org.teapot.security.payload.SignUpRequest;
 import org.teapot.security.repository.UserRepository;
 import org.teapot.security.security.TokenProvider;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/auth", consumes = "application/json", produces = "application/json")
@@ -46,7 +40,7 @@ public class AuthController {
 
     @PostMapping("/login")
     @ApiOperation(value = "Local login with email and password")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public AuthResponse authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getEmail(),
@@ -57,20 +51,14 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String token = tokenProvider.createToken(authentication);
-        return ResponseEntity.ok(new AuthResponse(token));
+        return new AuthResponse(token);
     }
 
     @PostMapping("/signup")
     @ApiOperation(value = "Local sign up with email and password")
-    @ApiResponses({
-            @ApiResponse(
-                    code = HttpServletResponse.SC_CREATED,
-                    message = "Successfully signed up",
-                    response = Response.class
-            ),
-            @ApiResponse(code = HttpServletResponse.SC_BAD_REQUEST, message = "User already exists")
-    })
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses(@ApiResponse(code = HttpServletResponse.SC_BAD_REQUEST, message = "User already exists"))
+    public void registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
             throw new BadRequestException("Email address already in use.");
         }
@@ -81,16 +69,7 @@ public class AuthController {
         user.setEmail(signUpRequest.getEmail());
         user.setPassword(signUpRequest.getPassword());
         user.setProvider(AuthProvider.local);
-
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        User result = userRepository.save(user);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentContextPath().path("/user/me")
-                .buildAndExpand(result.getId()).toUri();
-
-        return ResponseEntity.created(location)
-                .body(new Response(true, "User registered successfully@"));
+        userRepository.save(user);
     }
 }
